669c8270253793d9bd4eacf0bdfb0a95e9abe3bf
southcat <admin@southcat.net>
2021-12-21 21:53:34 +0800

    添加正则匹配

--- app.js
+++ app.js
@@ -1,152 +1,144 @@
-App({
-  onLaunch: function () {
-    this.getUserInfo();
+
+//index.js
+//获取应用实例
+const app = getApp()
+const util = require('../../utils/util.js');
+const updateManager = wx.getUpdateManager()
+
+//版本更新
+updateManager.onCheckForUpdate(function (res) {
+  // 请求完新版本信息的回调
+  console.log(res.hasUpdate)
+})
+
+updateManager.onUpdateReady(function () {
+  wx.showModal({
+    title: '更新提示',
+    content: '新版本已经准备好，是否重启应用？',
+    success(res) {
+      if (res.confirm) {
+        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        updateManager.applyUpdate()
+      }
+    }
+  })
+})
+
+updateManager.onUpdateFailed(function () {
+  // 新版本下载失败
+})
+
+Page({
+  data: {
+    userInfo: {},
+    videoUrl: 'https://v.douyin.com/JxHkvPT/',
   },
 
-  /**
-   * 登陆并获取用户信息、token
-   * @param {*} callback 
-   */
-  getUserInfo: function(callback = null) {
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        var code = res.code;
-        // 获取用户信息
-        wx.getSetting({
-          success: res => {
-            if (res.authSetting['scope.userInfo']) {
-              console.log(1)
-              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-              wx.getUserInfo({
-                success: res => {
-                  console.log(2)
-                  // 可以将 res 发送给后台解码出 unionId
-                  this.globalData.userInfo = res.userInfo
-                  this.globalData.hasUserInfo = true
-                  if (!this.checkIsLogin()) {
-                    console.log(3)
-                    this.getToken(code, res.encryptedData, res.iv);
-                  }
+  onLoad: function () {
+  
+  },
 
-                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                  // 所以此处加入 callback 以防止这种情况
-                  callback && callback(res);
-                }
+  onShow() {
+    // 如果剪切板内有内容则尝试自动填充
+    wx.getClipboardData({ success: res => {
+      var str = res.data.trim()
+      if (this.regUrl(str)) {
+        wx.showModal({
+          title: '检测到剪切板有视频地址，是否自动填入？',
+          success: res => {
+            if (res.confirm) {
+              var reg= /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
+              str = str.match(reg);
+              str = str[0]
+              this.setData({
+                videoUrl: str
               })
             }
           }
         })
       }
-    });
+    } })
   },
 
-  globalData: {
-    userInfo: null,
-    hasUserInfo: false,
-    apiDomain: 'http://127.0.0.1:8000/api', //生产
-    downloadPrefix: 'http://127.0.0.1:8000/download?url=', // 通过代理服务器中转（微信限制资源域名，不同平台cdn域名千变万化）
-    defaultDailyFreeParseNum: 10,
+  // 清空输入框
+  inputClear: function () {
+    this.setData({
+      videoUrl: ''
+    })
   },
 
-  //全局统一调用接口的方法
-  apiRequest: function (options) {
-    wx.request({
-      url: this.globalData.apiDomain + options.url,
-      method: options.method ? options.method : 'GET',
-      header: {
-        'Authorization': 'Bearer ' + wx.getStorageSync('token'),
-        'Accept': 'application/json',
-      },
-      dataType: 'json',
-      data: options.data,
-      success: res => {
-        switch (res.statusCode) {
-          case 200:
-            options.success(res);
-            break;
-          case 401:
-            this.toLogin();
-            break;
-          case 422:
-            break;
-          case 404:
-            wx.showToast({
-              title: '请求地址不存在',
-              icon: 'none'
-            })
-            break;
-          default:
-            wx.showToast({
-              title: '出错了～请稍后再试',
-              icon: 'none'
-            })
-        }
-      },
-      fail: res => {
-        if (options.fail) {
-          options.fail(res);
-        }
-      },
-      complete: res => {
-        if (options.complete) {
-          options.complete(res);
-        }
-      }
-    });
+  // 视频地址匹配是否合法
+  regUrl: function (t) {
+    return /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(t)
   },
 
-  /**
-   * 验证登录
-   */
-  checkIsLogin(autoLogin = false) {
-    if (wx.getStorageSync('token') != '') {
-      return true;
-    }
-    if (autoLogin) {
-      this.toLogin();
+  submit: function() {
+    var num;
+    var today = util.formatDate(new Date(), '');
+    var lastParseDate = wx.getStorageSync('lastParseDate');
+    if (lastParseDate != today) {
+      wx.setStorageSync('lastParseDate', today);
+      wx.setStorageSync('dailyFreeParseNum', app.globalData.defaultDailyFreeParseNum);
+      num = app.globalData.defaultDailyFreeParseNum;
+      // num = 100
     } else {
-      return false;
+      num = wx.getStorageSync('dailyFreeParseNum');
+      // num = 100
+    }
+    if (num > 0) {
+      this.parseVideo();
+    } else {
+      wx.showToast({
+        title: '免费解析次数已用完！',
+        icon: 'none'
+      })
+      // // 超免费次数需要观看激励广告
+      // wx.showModal({
+      //   title: "解析视频",
+      //   content: "免费解析次数已用完，需观看完广告才可继续解析！",
+      //   confirmColor: "#00B269",
+      //   cancelColor: "#858585",
+      //   success: (res) => {
+      //     if (res.confirm) {
+      //       videoAd.show().catch(() => {
+      //         // 失败重试
+      //         videoAd.load()
+      //           .then(() => videoAd.show())
+      //           .catch(err => {
+      //             console.log('激励视频 广告显示失败')
+      //           })
+      //       })
+      //     } else if (res.cancel) {
+      //       wx.showToast({
+      //         title: '广告观看完才可继续解析！',
+      //         icon: 'none'
+      //       })
+      //     }
+      //   }
+      // })
     }
   },
 
-  /**
-   * 跳转登陆页
-   */
-  toLogin() {
-    this.globalData.hasUserInfo = false;
-    this.globalData.userInfo = null;
-    wx.removeStorageSync('token');
-    wx.showToast({
-      title: '请先登陆!',
-      icon: 'none',
+  // 视频解析
+  parseVideo: function () {
+    app.apiRequest({
+      url: '/video-parse',
+      method: 'POST',
+      data: {
+        url: this.data.videoUrl
+      },
       success: res => {
-        wx.switchTab({
-          url: '/pages/mine/mine'
+        var noWaterUrl = encodeURIComponent(res.data.url);
+        var imageUrl = encodeURIComponent(res.data.image);
+        var preview = res.data.preview;
+        wx.setStorageSync('dailyFreeParseNum',  wx.getStorageSync('dailyFreeParseNum') - 1);
+        wx.navigateTo({
+          url: "../video/video?url=" + noWaterUrl + '&image=' + imageUrl + '&preview=' + preview,
         })
       }
     })
   },
 
-  /**
-   * 获取token
-   */
-  getToken(code, encryptedData, iv, callback = null) {
-    //调后端接口获取token
-    this.apiRequest({
-      url: '/auth/login',
-      method: 'POST',
-      data: {
-        'code': code,
-        'data': encryptedData,
-        'iv': iv
-      },
-      success: res => {
-        wx.setStorageSync('token', res.data.token);
-        callback && callback();
-      }
-    });
-  },
+  //版本检测更新
   
-});
+})
